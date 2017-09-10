90e7d59bce3c04c01377612eaf031a79ffc0ec50
Takehaya Susanoo <tyousufleo8@gmail.com>
2017-09-09 15:38:49 -1000

    Made a tree

---
+++ SunderingClaws.py
@@ -0,0 +1,78 @@
+class Node:
+    def __init__(self, id, parent, value):
+        self.id = id
+        self.parent = parent
+        self.child1 = None
+        self.child2 = None
+        self.value = value
+
+class Ao:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, id, value):
+        if self.root is None:
+            self.root = Node(id, None, value)
+        else:
+            return put_in(self.root, id, value)
+
+    def find(self, id):
+        return roar(self.root, id)
+
+    def traverse(self, command):
+        splintering_thrust(self.root, command)
+
+def roar(node, id):
+    if node is None:
+        return None
+    if id > node.id:
+        return roar(node.child2, id)
+    if id < node.id:
+        return roar(node.child1, id)
+    if id == node.id:
+        return node.value
+
+def splintering_thrust(node, command):
+    if node is None:
+        return
+    splintering_thrust(node.child1, command)
+    command(node.id, node.value)
+    splintering_thrust(node.child2, command)
+
+def put_in(node, id, value):
+    if id > node.id:
+        if node.child2 is None:
+            node.child2 = Node(id, node, value)
+        else:
+            return put_in(node.child2, id, value)
+
+    elif id < node.id:
+        if node.child1 is None:
+            node.child1 = Node(id, node, value)
+        else:
+            return put_in(node.child1, id, value)
+
+    else:
+        old_value = node.value
+        node.value = value
+        return old_value
+
+def main():
+    my_tree = Ao()
+    my_tree.insert(5, "Ichishiki!")
+    print(my_tree.insert(5, "Ichishiki"))
+    my_tree.insert(2, "Nishiki")
+    my_tree.insert(7, "Sanshiki")
+    my_tree.insert(6, "Yonshiki")
+    my_tree.insert(9, "Rokushiki")
+    my_tree.insert(12, "Nanashiki")
+    my_tree.insert(10, "Hachishiki")
+    my_tree.insert(15, "Kowasu")
+    my_tree.insert(13, "Akkemai")
+    my_tree.insert(16, "Kore de owari ka?")
+    my_tree.insert(21, "Ware wa... subete wo owaraseru!")
+    my_tree.insert(18, "MESSATSU!!!")
+    my_tree.traverse(print)
+    print(my_tree.find(2))
+main()
+
